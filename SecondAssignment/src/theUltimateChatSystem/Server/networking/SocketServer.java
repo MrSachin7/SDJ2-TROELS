8b13fc9fa81891b836277e3de078c482288ea1e0
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-10-12 11:18:08 +0200

    SOooooo cleannnnnnn

--- SecondAssignment/src/theUltimateChatSystem/Server/networking/SocketServer.java
+++ SecondAssignment/src/theUltimateChatSystem/Server/networking/SocketServer.java
@@ -1,7 +1,7 @@
 package theUltimateChatSystem.Server.networking;
 
-import theUltimateChatSystem.Server.model.LoginHandler;
 import theUltimateChatSystem.Server.model.ChatHandler;
+import theUltimateChatSystem.Server.model.LoginHandler;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -14,7 +14,7 @@ public class SocketServer {
 
     public SocketServer(ChatHandler model, LoginHandler loginHandler) {
         this.model = model;
-        this.loginHandler=loginHandler;
+        this.loginHandler = loginHandler;
 
     }
 
@@ -26,13 +26,13 @@ public class SocketServer {
             while (true) {
                 System.out.println("Waiting for clients.....");
                 Socket socket = welcomeSocket.accept();
-              //  System.out.println(socket.getInetAddress().getHostAddress() + "  identified");
-                ServerHandler serverHandler = new ServerHandler(socket, model,loginHandler,cp);
-             //   System.out.println("A new server handler is created");
-               // cp.addConnection(serverHandler);
+                //  System.out.println(socket.getInetAddress().getHostAddress() + "  identified");
+                ServerHandler serverHandler = new ServerHandler(socket, model, loginHandler, cp);
+                // System.out.println("A new server handler is created");
+                // cp.addConnection(serverHandler);
                 System.out.println(cp.size());
-               Thread t = new Thread(serverHandler);
-               t.start();
+                Thread t = new Thread(serverHandler);
+                t.start();
             }
 
         } catch (IOException e) {

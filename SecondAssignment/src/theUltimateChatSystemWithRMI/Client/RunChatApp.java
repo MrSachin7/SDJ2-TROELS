6e540af9d45bc9565b91f2c3cc542029b2c6202f
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-11-06 01:51:01 +0100

    Delete

--- SecondAssignment/src/theUltimateChatSystemWithRMI/Client/RunChatApp.java
+++
@@ -1,9 +0,0 @@
-package theUltimateChatSystemWithRMI.Client;
-
-import javafx.application.Application;
-
-public class RunChatApp {
-    public static void main(String[] args) {
-         Application.launch(ChatApp.class);
-    }
-}
